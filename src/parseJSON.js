6b52c98b2272973294fa719623208c123c7d2d81
William Simmons <willsimmons@gmail.com>
2016-09-13 08:30:17 -0700

    just some formatting

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -5,56 +5,56 @@
 var parseJSON = function(json) {
   // your code goes here
   function testBool(string) {
-    if (string==="true") {
-      return true
+    if (string === "true") {
+      return true;
     }
-    else if (string==="false") {
-      return false
+    else if (string === "false") {
+      return false;
     }
-    else if (string==="null") {
-      return null
+    else if (string === "null") {
+      return null;
     }
     else {
-      return "number"
+      return "number";
     }
   }
   //test for easy 1 item Json and just return that
-  if(json[0]==='"'){
-    if (json==='""') {
+  if (json[0] === '"') {
+    if (json === '""') {
       return '';
     }
-    var string=json.slice(1,json.length-1).split('')
+    var string = json.slice(1, json.length - 1).split('');
     var escaped = false;
     var toDelete = [];
-    string.forEach(function(letter,index){
-      if(letter === "\\" && escaped === false){
+    string.forEach(function(letter, index) {
+      if (letter === "\\" && escaped === false) {
         escaped = true;
         toDelete.push(index);
       }
-      else if(escaped ===true){
+      else if (escaped === true) {
         //just in case
-        escaped=false;
+        escaped = false;
       }
     });
-    return string.filter(function(x,i){
+    return string.filter(function(x, i) {
       if (toDelete.includes(i)) {
-        return false
+        return false;
       } else {
-        return true
+        return true;
       }
-    }).join('')
+    }).join('');
   }
-  else if (json[0]==='[') {
+  else if (json[0] === '[') {
     //handle arrays
     var arr = json.split("");
     //handling empty arrays
-    if (arr[1]==="]"){
+    if (arr[1] === "]"){
       return [];
     } 
     arr.pop();
     arr.shift();
 
-    var commaIndexes = []
+    var commaIndexes = [];
     var spaceIndexes = [];
 
     var skippingObject = false;
@@ -64,93 +64,93 @@ var parseJSON = function(json) {
     var count = 0;
     //handle array
     //split the string -gathering all indices within our JSON string
-    arr.forEach(function(x, index){
+    arr.forEach(function(x, index) {
        //if we run into [,{," start skipping until that value ends
        //handling array's
-      if(afterComma === true && skippingString === false){
-        if (x===" ") {
-          spaceIndexes.push(1)
+       if (afterComma === true && skippingString === false){
+        if (x === " ") {
+          spaceIndexes.push(1);
         }
         else {
-          spaceIndexes.push(0)
+          spaceIndexes.push(0);
         }
 
       }
-      if(skippingString === true){
-        if(x==='"'){
+      if (skippingString === true){
+        if (x === '"'){
           skippingString = false;
         }
       }
-       else if (skippingArray===true) {
-          if (x ==='"'){
-            skippingString = true;
-          }
-          else if (x==='[') {
-            count++;
-          } 
-          else if (count===0 && x==="]") {
-            skippingArray = false;
-          }
-          else if (count!==0 && x==="[") {
-            count--;
-          }
-       }
-       else if (skippingObject===true) {
-          if (x ==='"'){
-            skippingString = true;
-          }
-          else if (x==='{') {
-            count++;
-          } 
-          else if (count===0 && x==="}") {
-            skippingObject = false;
-          }
-          else if (count!==0 && x==="{") {
-            count--;
-          }
-       }
-       else if(x ==='{'){
+      else if (skippingArray === true) {
+        if (x ==='"'){
+          skippingString = true;
+        }
+        else if (x ==='[') {
+          count++;
+        } 
+        else if (count === 0 && x === "]") {
+          skippingArray = false;
+        }
+        else if (count !== 0 && x === "[") {
+          count--;
+        }
+      }
+      else if (skippingObject === true) {
+        if (x ==='"'){
+          skippingString = true;
+        }
+        else if (x==='{') {
+          count++;
+        } 
+        else if ( count ===0 && x==="}") {
+          skippingObject = false;
+        }
+        else if (count !==0 && x==="{") {
+          count--;
+        }
+      }
+      else if(x ==='{'){
         skippingObject = true;
-       }
-       else if(x ==='['){
+      }
+      else if(x ==='['){
         skippingArray = true;
-       }
-       else if(x===','){
+      }
+      else if(x ===','){
         commaIndexes.push(index);
         afterComma = true;
-       }
+      }
 
     });
 
-     var result = [];
+    var result = [];
 
-     arr = arr.join('')
+    arr = arr.join('')
 
-     
 
-     if (commaIndexes.length === 0){
+
+    if (commaIndexes.length === 0) {
       return [parseJSON(arr)]
+    }
+
+    commaIndexes.push(arr.length);
+
+
+    commaIndexes.forEach(function(x, i){
+      if (i === 0){
+       result[i] = parseJSON(arr.slice(0, x));
+     }else {
+       result[i] = parseJSON(arr.slice(commaIndexes[i-1] + 1 + spaceIndexes[i-1],x));
      }
-
-     commaIndexes.push(arr.length);
-
-
-  commaIndexes.forEach(function(x,i){
-    if (i===0){
-     result[i] = parseJSON(arr.slice(0,x));
-    }else {
-     result[i] = parseJSON(arr.slice(commaIndexes[i-1] + 1+spaceIndexes[i-1],x));
-   }
-  });
+   });
     
-     
-    return result
+
+    return result;
 
   }
-  else if (json[0]==='{') {
+  else if (json[0] === '{') {
     //handle object
-    if (json[1]==="}") {
-      return {}
+    if (json[1] === "}") {
+      return {};
     }
     var obj = json.split("");
     obj.pop();
@@ -164,101 +164,101 @@ var parseJSON = function(json) {
 
     var keyParse = false;
     var valueParse = false;
-    var afterComma = false
+    var afterComma = false;
 
     var keys = [];
     var values = [];
     var afterCommas = [];
 
 
-    obj.forEach(function(x,i){
-      if (afterComma===true) {
+    obj.forEach(function(x, i){
+      if (afterComma === true) {
         if (x===" ") {
           afterCommas.push(1);
-          afterComma = false
+          afterComma = false;
         }
         else {
-          afterCommas.push(0)
+          afterCommas.push(0);
           afterComma = false;
         }
       }
-      if (valueParse===true) {
+      if (valueParse === true) {
         if(skippingString === true){
-        if(x==='"'){
-          skippingString = false;
+          if(x ==='"'){
+            skippingString = false;
+          }
         }
-      }
-       else if (skippingArray===true) {
+        else if (skippingArray === true) {
           if (x ==='"'){
             skippingString = true;
           }
-          else if (x==='[') {
+          else if (x ==='[') {
             count++;
           } 
-          else if (count===0 && x==="]") {
+          else if (count === 0 && x === "]") {
             skippingArray = false;
           }
-          else if (count!==0 && x==="[") {
+          else if (count !== 0 && x === "[") {
             count--;
           }
-       }
-       else if (skippingObject===true) {
+        }
+        else if (skippingObject === true) {
           if (x ==='"'){
             skippingString = true;
           }
-          else if (x==='{') {
+          else if (x ==='{') {
             count++;
           } 
-          else if (count===0 && x==="}") {
+          else if (count === 0 && x === "}") {
             skippingObject = false;
           }
-          else if (count!==0 && x==="{") {
+          else if (count !== 0 && x === "{") {
             count--;
           }
-       }
-       else if(x ==='{'){
-        skippingObject = true;
-       }
-       else if(x ==='['){
-        skippingArray = true;
-       }
-       else if (x === ',') {
-        values[values.length-1].push(i);
-        valueParse = false
-       }
+        }
+        else if(x ==='{'){
+          skippingObject = true;
+        }
+        else if(x === '['){
+          skippingArray = true;
+        }
+        else if (x === ',') {
+          values[values.length-1].push(i);
+          valueParse = false;
+        }
       }
-      if (keyParse===false) {
-        if (x===":"){
+      if (keyParse === false) {
+        if (x === ":") {
           valueParse = true;
           values.push([i+1]);
           afterComma = true;
           //values.push([i+2])
         }
-        if(x === '"' && valueParse===false){
+        if (x === '"' && valueParse === false) {
           keyParse = true;
-          keys.push([i+1])
+          keys.push([i+1]);
         }
       }
-      else if (keyParse===true) {
-        if (x==='"') {
+      else if (keyParse === true) {
+        if (x ==='"') {
           keys[keys.length-1].push(i);
           keyParse = false;
         }
       }
     })
-    if (obj[obj.length-1] === " "){
+    if (obj[obj.length-1] === " ") {
      obj.pop();
-    }
-    obj=obj.join('')
-    values[values.length-1].push(obj.length)
-    var result = {}
-    keys.forEach(function(objKeys,index){
-      result[obj.slice(objKeys[0],objKeys[1])] = parseJSON(obj.slice(values[index][0]+afterCommas[index],values[index][1]))
-    })
-    console.log(result);
-    return result
-  }
-  else {
+   }
+   obj = obj.join('');
+   values[values.length-1].push(obj.length);
+   var result = {};
+   keys.forEach(function(objKeys, index){
+    result[obj.slice(objKeys[0],objKeys[1])] = parseJSON(obj.slice(values[index][0]+afterCommas[index],values[index][1]))
+  });
+   console.log(result);
+   return result;
+ }
+ else {
     //handle values
     if (testBool(json) === "number"){
       return parseFloat(json);
@@ -269,7 +269,7 @@ var parseJSON = function(json) {
   }
 };
 
-/* 
+/* what we are currently working on
 [' +
     '{"documentation":"Displays a dialog box that allows user to ' +
     'select a folder on the local system.","name":' +
